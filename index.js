0634917932853226d660c20b33dd97a509fca1cc
brunomgferreira <brunomgferreira17@gmail.com>
2024-03-30 13:00:44 +0000

    Landing page and menu code was cleaned

--- index.js
+++ index.js
@@ -1,58 +1,51 @@
-const main = document.querySelector("main");
-const menu = document.querySelector("menu");
-const menuNavUl = document.querySelector("menu nav ul");
-const openButton = document.getElementById("menu-icon");
-const closeButton = document.getElementById("close-icon");
+import { openButton, closeButton, openMenu, closeMenu } from "./menu.js";
 
-const quoteContainer = document.getElementsByClassName("quote-container")[0];
-const mainNav = quoteContainer.nextElementSibling;
+const landingPage = (() => {
+  const quoteContainer = document.getElementsByClassName("quote-container")[0];
+  const mainNav = quoteContainer.nextElementSibling;
 
-const openMenu = () => {
-  openButton.classList.add("hidden");
-  closeButton.classList.remove("hidden");
+  const openMenuLandingPage = () => {
+    quoteContainer.classList.remove("quote-appear");
+    mainNav.classList.remove("main-nav-appear");
 
-  quoteContainer.classList.remove("quote-appear");
-  mainNav.classList.remove("main-nav-appear");
+    quoteContainer.offsetWidth;
+    mainNav.offsetWidth;
 
-  quoteContainer.offsetWidth;
-  mainNav.offsetWidth;
+    quoteContainer.classList.add("quote-disappear");
+    mainNav.classList.add("main-nav-disappear");
 
-  quoteContainer.classList.add("quote-disappear");
-  mainNav.classList.add("main-nav-disappear");
+    openMenu();
+  };
 
-  menuNavUl.querySelectorAll("li").forEach((li) => {
-    li.classList.remove("menu-nav-ul-li-disappear");
-    li.offsetWidth;
-    li.classList.add("menu-nav-ul-li-appear");
-  });
+  const closeMenuLandingPage = () => {
+    quoteContainer.classList.remove("quote-disappear");
+    mainNav.classList.remove("main-nav-disappear");
 
-  setTimeout(() => {
-    menu.classList.remove("hidden");
-    main.classList.add("hidden");
-  }, 310);
-};
+    quoteContainer.offsetWidth;
+    mainNav.offsetWidth;
 
-const closeMenu = () => {
-  openButton.classList.remove("hidden");
-  closeButton.classList.add("hidden");
+    quoteContainer.classList.add("quote-appear");
+    mainNav.classList.add("main-nav-appear");
 
-  quoteContainer.classList.remove("quote-disappear");
-  mainNav.classList.remove("main-nav-disappear");
+    closeMenu();
+  };
 
-  quoteContainer.offsetWidth;
-  mainNav.offsetWidth;
+  return {
+    openMenuLandingPage,
+    closeMenuLandingPage,
+  };
+})();
 
-  quoteContainer.classList.add("quote-appear");
-  mainNav.classList.add("main-nav-appear");
+openButton.addEventListener("click", () => {
+  landingPage.openMenuLandingPage();
+});
 
-  menuNavUl.querySelectorAll("li").forEach((li) => {
-    li.classList.remove("menu-nav-ul-li-appear");
-    li.offsetWidth;
-    li.classList.add("menu-nav-ul-li-disappear");
-  });
+closeButton.addEventListener("click", () => {
+  landingPage.closeMenuLandingPage();
+});
 
-  setTimeout(() => {
-    menu.classList.add("hidden");
-    main.classList.remove("hidden");
-  }, 410);
-};
+const headerName = document.getElementById("header-name");
+
+headerName.addEventListener("click", () => {
+  landingPage.closeMenuLandingPage();
+});
