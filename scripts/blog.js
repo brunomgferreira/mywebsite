8fbdea78f4e8d2ba0a46ad50067056e48d5c53c0
brunomgferreira <brunomgferreira17@gmail.com>
2024-04-03 06:58:03 +0100

    Blog page is working

--- scripts/blog.js
+++ scripts/blog.js
@@ -5,13 +5,27 @@ import {
   closeMenu as closeMenuFunction,
 } from "./menu.js";
 
+const main = document.querySelector("body > main");
 const mainTitle = document.querySelector("main > header > div > h1");
 const mainQuote = document.querySelector(
   "main > header > div > p:nth-of-type(1)"
 );
 
-const articles = document.querySelectorAll("article");
-const articlesText = document.querySelectorAll("article > div > p");
+let articles = document.querySelectorAll("article");
+let articlesText = document.querySelectorAll("article > div > p");
+
+const posts = [
+  {
+    title: "FastAI - Chapter 1 & 2",
+    date: "01/04/2024",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure delectus eum, nihil deleniti dolorum, a reiciendis asperiores sint omnis ipsa ad necessitatibus facere? Magnam architecto cupiditate id vero totam eum!",
+  },
+  {
+    title: "FastAI - Chapter 1 & 2",
+    date: "01/04/2024",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure delectus eum, nihil deleniti dolorum, a reiciendis asperiores sint omnis ipsa ad necessitatibus facere? Magnam architecto cupiditate id vero totam eum!",
+  },
+];
 
 const headerAppear = () => {
   mainTitle.classList.remove("opacity-disappear");
@@ -100,6 +114,30 @@ closeButton.addEventListener("click", () => {
   closeMenu();
 });
 
+const createPostElement = (post) => {
+  const article = document.createElement("article");
+  article.innerHTML = `
+    <header>
+        <h2>${post.title}</h2>
+        <p>${post.date}</p>
+    </header>
+    <div>
+        <p>${post.info}</p>
+    </div>
+  `;
+  return article;
+};
+
+const renderPosts = () => {
+  posts.forEach((post) => {
+    const postElement = createPostElement(post);
+    main.appendChild(postElement);
+  });
+
+  articles = document.querySelectorAll("article");
+  articlesText = document.querySelectorAll("article > div > p");
+};
+
 const headerName = document.getElementById("header-name");
 
 headerName.addEventListener("click", () => {
@@ -111,5 +149,6 @@ headerName.addEventListener("click", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
+  renderPosts();
   openMain();
 });
